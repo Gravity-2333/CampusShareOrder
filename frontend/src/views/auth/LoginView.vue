a4c175ca1238b095a1ee8855b59b93534097c750
Gravity-2333 <yhf248655@163.com>
2026-04-12 15:56:20 +0800

    前端Vue3框架初步搭建

---
+++ frontend/src/views/auth/LoginView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="login-container">
+      <h2>Login Page</h2>
+      <p>项目已成功启动</p>
+    </div>
+  </template>
+  
+  <script setup>
+  </script>
+  
+  <style scoped>
+  .login-container {
+    padding: 40px;
+    text-align: center;
+  }
+  </style>
